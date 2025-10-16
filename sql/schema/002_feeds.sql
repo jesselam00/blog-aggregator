f0cdc1a87b086a013abfb1f395ee1a3d168ce887
Jesse Lam <jesselam00@Mac.home.local>
2025-10-16 15:00:05 -0500

    Refactor addfeed handler and reorganize feeds schema/queries

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -3,10 +3,9 @@ CREATE TABLE feeds (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
     url TEXT NOT NULL UNIQUE,
-    user_id UUID NOT NULL,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
